732047e02b4b4de3b0326d40c7904256e7996549
Diego Antonio De León Enríquez <dele24247@uvg.edu.gt>
2025-02-25 20:27:25 -0600

    Update AbstractList.java

--- src/main/java/uvg/edu/gt/AbstractList.java
+++ src/main/java/uvg/edu/gt/AbstractList.java
@@ -1,10 +1,9 @@
 package uvg.edu.gt;
 
 /**
- * Clase abstracta que proporciona la estructura base para la implementación de una lista.
+ * Clase abstracta que proporciona la estructura base para la implementación de una lista
  *
- * @param <T> Tipo de datos que almacenará la lista.
+ * @param <T> Tipo de datos que almacenará la lista
  */
 public abstract class AbstractList<T> implements IList<T> {
-    // Las clases concretas implementarán los métodos de la interfaz IList
 }
