822aeb24b22535416c4d5918658967028941126f
Diego Antonio De León Enríquez <dele24247@uvg.edu.gt>
2025-02-25 20:18:06 -0600

    Update AbstractStack.java

--- src/main/java/uvg/edu/gt/AbstractStack.java
+++ src/main/java/uvg/edu/gt/AbstractStack.java
@@ -1,5 +1,10 @@
 package uvg.edu.gt;
 
+/**
+ * Clase abstracta que proporciona la estructura base para la implementación de una pila (Stack).
+ *
+ * @param <T> Tipo de datos que almacenará la pila.
+ */
 public abstract class AbstractStack<T> implements IStack<T> {
-    // Métodos de la interfaz IStack serán implementados por las clases concretas
+    // Las clases concretas implementarán los métodos de la interfaz IStack
 }
