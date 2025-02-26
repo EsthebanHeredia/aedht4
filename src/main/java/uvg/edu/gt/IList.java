bf6e326ccbfc39fd89c9819960adc1011b355b01
Diego Antonio De León Enríquez <dele24247@uvg.edu.gt>
2025-02-25 20:36:08 -0600

    Update IList.java

--- src/main/java/uvg/edu/gt/IList.java
+++ src/main/java/uvg/edu/gt/IList.java
@@ -1,41 +1,41 @@
 package uvg.edu.gt;
 
 /**
- * Interfaz que define las operaciones de una lista, extendiendo las funcionalidades de una pila.
+ * Interfaz que define las operaciones de una lista, extiende funciones de una pila
  *
- * @param <T> Tipo de datos que almacenará la lista.
+ * @param <T> Tipo de datos que almacenará dicha lista
  */
 public interface IList<T> extends IStack<T> {
 
     /**
-     * Agrega un elemento al final de la lista.
+     * Agrega un elemento al final de la lista
      *
-     * @param element Elemento a agregar.
+     * @param element lo que se agrega
      */
     void add(T element);
 
     /**
-     * Elimina y devuelve un elemento en una posición específica de la lista.
+     * Elimina y devuelve un elemento en una posición específica 
      *
-     * @param index Índice del elemento a eliminar.
-     * @return Elemento eliminado.
-     * @throws IndexOutOfBoundsException Si el índice está fuera de los límites de la lista.
+     * @param index Índice del elemento a eliminar
+     * @return Elemento lo que se elimina
+     * @throws IndexOutOfBoundsException Si el índice está fuera de los límites de la lista
      */
     T remove(int index);
 
     /**
-     * Obtiene un elemento en una posición específica de la lista sin eliminarlo.
+     * Obtiene un elemento en una posición específica de la lista sin eliminarlo
      *
-     * @param index Índice del elemento a obtener.
-     * @return Elemento en la posición indicada.
-     * @throws IndexOutOfBoundsException Si el índice está fuera de los límites de la lista.
+     * @param index 
+     * @return Elemento .
+     * @throws IndexOutOfBoundsException 
      */
     T get(int index);
 
     /**
-     * Obtiene el número de elementos en la lista.
+     * Obtiene el número de elementos en la lista
      *
-     * @return Tamaño de la lista.
+     * @return Tamaño de la lista
      */
     int size();
 
