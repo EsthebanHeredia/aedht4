fd1b9a1eb62721754d91c0830d5d3f8b49b05e17
Diego Antonio De León Enríquez <dele24247@uvg.edu.gt>
2025-02-25 20:14:57 -0600

    Update ICalculator.java

--- src/main/java/uvg/edu/gt/ICalculator.java
+++ src/main/java/uvg/edu/gt/ICalculator.java
@@ -1,5 +1,16 @@
 package uvg.edu.gt;
 
+/**
+ * Interfaz que define la funcionalidad de una calculadora capaz de evaluar expresiones.
+ */
 public interface ICalculator {
-    int evaluate(String var1) throws Exception;
+
+    /**
+     * Evalúa una expresión matemática y devuelve el resultado.
+     *
+     * @param expression Expresión en formato postfix a evaluar.
+     * @return Resultado de la evaluación.
+     * @throws Exception Si la expresión es inválida o hay un error en la operación.
+     */
+    int evaluate(String expression) throws Exception;
 }
